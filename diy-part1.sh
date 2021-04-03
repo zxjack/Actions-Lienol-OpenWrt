2a87a0338040c2b76761714211af6e51b5e3d26e
lxhao61 <lxhao61@gmail.com>
2021-04-03 09:28:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,3 @@
 
 # 添加xiaorouji大passwall源
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/lxhao61/openwrt-passwall.git;main' feeds.conf.default
